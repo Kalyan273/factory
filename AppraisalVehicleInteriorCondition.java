723eb32dc513d10ce68678eb3a675607ecd74c5a
Kalyan273 <107022856+Kalyan273@users.noreply.github.com>
2023-02-15 18:10:11 +0530

    Add files via upload

---
+++ AppraisalVehicleInteriorCondition.java
@@ -0,0 +1,201 @@
+package com.factory.appraisal.vehiclesearchapp.persistence.model;
+
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Audited
+@Entity
+@Table(name="FACTORY_DB.APR_VEH_INTR_CONDN")
+@DynamicUpdate
+@DynamicInsert
+@AttributeOverride(name = "valid", column = @Column(name = "IS_ACTIVE"))
+public class AppraisalVehicleInteriorCondition extends TransactionEntity{
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    @Column(name = "INTR_CONDN_ID")
+    @Min(1)
+    @Max(99999999999L)
+    private Long interiorConditionId ;
+
+    @OneToOne(targetEntity = EAppraisalTestDrivingStatus.class,fetch = FetchType.LAZY)
+    @JoinColumn(name = "VEH_STATUS_ID",referencedColumnName = "")
+    @Max(15)
+    private EAppraisalTestDrivingStatus vehicleStatusId;
+
+    @Column(name = "CLEAN_FL")
+    private boolean cleanFrontlineReady ;
+    @Column(name = "GOOD_MNR_RPR_ND")
+    private boolean goodMinorRepairsNeeded ;
+    @Column(name = "SMOKERS_CAR")
+    private boolean smokersCar ;
+    @Column(name = "ODD_SMELL")
+    private boolean oddSmelling ;
+    @Column(name = "VERY_DIRTY")
+    private boolean veryDirty ;
+    @Column(name = "STG_PET_SMELL")
+    private boolean strongPetSmell ;
+    @Column(name = "DRV_SEAT_WEAR")
+    private boolean driversSeatWear ;
+    @Column(name = "HL_NEED_RPLC")
+    private boolean headlinearNeedReplacement ;
+    @Column(name = "DRV_SEAT_RIPPED")
+    private boolean driverSeatRipped;
+
+    @Column(name = "DASH_CRACKED_MNR")
+    private boolean dashCrackedMinor ;
+    @Column(name = "DASH_CRK_BRKN_MAJ")
+    private boolean dashCrackedBrokenMajor ;
+    @Column(name = "PAX_SEAT_RIPPED")
+    private boolean passengerSeatRipped ;
+    @Column(name = "CARPET_BADLY_WORN")
+    private boolean carpetBadlyWorn ;
+    @Column(name = "INT_TRIM_BRKN_MS")
+    private boolean interiorTrimBrokenMissing ;
+    @Column(name = "POOR_NEEDS_RPR")
+    private boolean poorNeedsRepair ;
+
+
+
+    public Long getInteriorConditionId() {
+        return interiorConditionId;
+    }
+
+    public void setInteriorConditionId(Long interiorConditionId) {
+        this.interiorConditionId = interiorConditionId;
+    }
+
+    public EAppraisalTestDrivingStatus getAppraisalStatusId() {
+        return appraisalStatusId;
+    }
+
+    public void setAppraisalStatusId(EAppraisalTestDrivingStatus appraisalStatusId) {
+        this.appraisalStatusId = appraisalStatusId;
+    }
+
+    public boolean isCleanFrontlineReady() {
+        return cleanFrontlineReady;
+    }
+
+    public void setCleanFrontlineReady(boolean cleanFrontlineReady) {
+        this.cleanFrontlineReady = cleanFrontlineReady;
+    }
+
+    public boolean isGoodMinorRepairsNeeded() {
+        return goodMinorRepairsNeeded;
+    }
+
+    public void setGoodMinorRepairsNeeded(boolean goodMinorRepairsNeeded) {
+        this.goodMinorRepairsNeeded = goodMinorRepairsNeeded;
+    }
+
+    public boolean isSmokersCar() {
+        return smokersCar;
+    }
+
+    public void setSmokersCar(boolean smokersCar) {
+        this.smokersCar = smokersCar;
+    }
+
+    public boolean isOddSmelling() {
+        return oddSmelling;
+    }
+
+    public void setOddSmelling(boolean oddSmelling) {
+        this.oddSmelling = oddSmelling;
+    }
+
+    public boolean isVeryDirty() {
+        return veryDirty;
+    }
+
+    public void setVeryDirty(boolean veryDirty) {
+        this.veryDirty = veryDirty;
+    }
+
+    public boolean isStrongPetSmell() {
+        return strongPetSmell;
+    }
+
+    public void setStrongPetSmell(boolean strongPetSmell) {
+        this.strongPetSmell = strongPetSmell;
+    }
+
+    public boolean isDriversSeatWear() {
+        return driversSeatWear;
+    }
+
+    public void setDriversSeatWear(boolean driversSeatWear) {
+        this.driversSeatWear = driversSeatWear;
+    }
+
+    public boolean isHeadlinearNeedReplacement() {
+        return headlinearNeedReplacement;
+    }
+
+    public void setHeadlinearNeedReplacement(boolean headlinearNeedReplacement) {
+        this.headlinearNeedReplacement = headlinearNeedReplacement;
+    }
+
+    public boolean isDriverSeatRipped() {
+        return driverSeatRipped;
+    }
+
+    public void setDriverSeatRipped(boolean driverSeatRipped) {
+        this.driverSeatRipped = driverSeatRipped;
+    }
+
+    public boolean isDashCrackedMinor() {
+        return dashCrackedMinor;
+    }
+
+    public void setDashCrackedMinor(boolean dashCrackedMinor) {
+        this.dashCrackedMinor = dashCrackedMinor;
+    }
+
+    public boolean isDashCrackedBrokenMajor() {
+        return dashCrackedBrokenMajor;
+    }
+
+    public void setDashCrackedBrokenMajor(boolean dashCrackedBrokenMajor) {
+        this.dashCrackedBrokenMajor = dashCrackedBrokenMajor;
+    }
+
+    public boolean isPassengerSeatRipped() {
+        return passengerSeatRipped;
+    }
+
+    public void setPassengerSeatRipped(boolean passengerSeatRipped) {
+        this.passengerSeatRipped = passengerSeatRipped;
+    }
+
+    public boolean isCarpetBadlyWorn() {
+        return carpetBadlyWorn;
+    }
+
+    public void setCarpetBadlyWorn(boolean carpetBadlyWorn) {
+        this.carpetBadlyWorn = carpetBadlyWorn;
+    }
+
+    public boolean isInteriorTrimBrokenMissing() {
+        return interiorTrimBrokenMissing;
+    }
+
+    public void setInteriorTrimBrokenMissing(boolean interiorTrimBrokenMissing) {
+        this.interiorTrimBrokenMissing = interiorTrimBrokenMissing;
+    }
+
+    public boolean isPoorNeedsRepair() {
+        return poorNeedsRepair;
+    }
+
+    public void setPoorNeedsRepair(boolean poorNeedsRepair) {
+        this.poorNeedsRepair = poorNeedsRepair;
+    }
+}
---
+++ AppraisalVehicleStereoStatus.java
@@ -0,0 +1,163 @@
+package com.factory.appraisal.vehiclesearchapp.persistence.model;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Audited
+@Entity
+@Table(name="FACTORY_DB.APR_VEH_STEREO_STATUS")
+@DynamicUpdate
+@DynamicInsert
+@AttributeOverride(name = "valid", column = @Column(name = "IS_ACTIVE"))
+public class AppraisalVehicleStereoStatus extends TransactionEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "STEREO_ST_ID")
+    @Min(1)
+    @Max(99999999999L)
+    private long stereoStatusId ;
+    @OneToOne(targetEntity = EAppraisalTestDrivingStatus.class,fetch = FetchType.LAZY)
+    @JoinColumn(name = "VEH_STATUS_ID",referencedColumnName = "")
+    private EAppraisalTestDrivingStatus vehicleStatusId;
+    @Column(name = "FACTORY_EQU_OP")
+    private boolean factoryEquipmentOperational ;
+    @Column(name = "EQPT_NOT_OP")
+    private boolean factoryEquipmentNotOperational ;
+    @Column(name = "KNOBS_MISSING")
+    private boolean knobsMissing ;
+    @Column(name = "AFT_MKT_NAV_NICE_SYS")
+    private boolean afterMarketNavigationNiceSystem ;
+    @Column(name = "AFT_MKT")
+    private boolean afterMarket;
+    @Column(name = "AFT_MKT_R_ENTMT_SYS")
+    private boolean aftermarketRearEntertainmentSystem ;
+    @Column(name = "FACTORY_R_ENTMT_SYS")
+    private boolean factoryRearEntertainmentSystem ;
+    @Column(name = "PRO_INSTALL")
+    private boolean professionalInstall ;
+    @Column(name = "BROKEN_SCREEN")
+    private boolean brokenScreen ;
+    @Column(name = "FADED_DIS_OR_BTNS")
+    private boolean fadedDisplayButtons ;
+    @Column(name = "NOT_OPRNL")
+    private boolean notOperational ;
+    @Column(name = "OPERATIONAL")
+    private boolean operational ;
+
+    public long getStereoStatusId() {
+        return stereoStatusId;
+    }
+
+    public void setStereoStatusId(long stereoStatusId) {
+        this.stereoStatusId = stereoStatusId;
+    }
+
+    public long getAPR_STATUS_ID() {
+        return appraisalStatusId;
+    }
+
+    public void setAPR_STATUS_ID(long APR_STATUS_ID) {
+        this.appraisalStatusId = APR_STATUS_ID;
+    }
+
+    public boolean isFactoryEquipmentOperational() {
+        return factoryEquipmentOperational;
+    }
+
+    public void setFactoryEquipmentOperational(boolean factoryEquipmentOperational) {
+        this.factoryEquipmentOperational = factoryEquipmentOperational;
+    }
+
+    public boolean isFactoryEquipmentNotOperational() {
+        return factoryEquipmentNotOperational;
+    }
+
+    public void setFactoryEquipmentNotOperational(boolean factoryEquipmentNotOperational) {
+        this.factoryEquipmentNotOperational = factoryEquipmentNotOperational;
+    }
+
+    public boolean isKnobsMissing() {
+        return knobsMissing;
+    }
+
+    public void setKnobsMissing(boolean knobsMissing) {
+        this.knobsMissing = knobsMissing;
+    }
+
+    public boolean isAfterMarketNavigationNiceSystem() {
+        return afterMarketNavigationNiceSystem;
+    }
+
+    public void setAfterMarketNavigationNiceSystem(boolean afterMarketNavigationNiceSystem) {
+        this.afterMarketNavigationNiceSystem = afterMarketNavigationNiceSystem;
+    }
+
+    public boolean isAfterMarket() {
+        return afterMarket;
+    }
+
+    public void setAfterMarket(boolean afterMarket) {
+        this.afterMarket = afterMarket;
+    }
+
+    public boolean isAftermarketRearEntertainmentSystem() {
+        return aftermarketRearEntertainmentSystem;
+    }
+
+    public void setAftermarketRearEntertainmentSystem(boolean aftermarketRearEntertainmentSystem) {
+        this.aftermarketRearEntertainmentSystem = aftermarketRearEntertainmentSystem;
+    }
+
+    public boolean isFactoryRearEntertainmentSystem() {
+        return factoryRearEntertainmentSystem;
+    }
+
+    public void setFactoryRearEntertainmentSystem(boolean factoryRearEntertainmentSystem) {
+        this.factoryRearEntertainmentSystem = factoryRearEntertainmentSystem;
+    }
+
+    public boolean isProfessionalInstall() {
+        return professionalInstall;
+    }
+
+    public void setProfessionalInstall(boolean professionalInstall) {
+        this.professionalInstall = professionalInstall;
+    }
+
+    public boolean isBrokenScreen() {
+        return brokenScreen;
+    }
+
+    public void setBrokenScreen(boolean brokenScreen) {
+        this.brokenScreen = brokenScreen;
+    }
+
+    public boolean isFadedDisplayButtons() {
+        return fadedDisplayButtons;
+    }
+
+    public void setFadedDisplayButtons(boolean fadedDisplayButtons) {
+        this.fadedDisplayButtons = fadedDisplayButtons;
+    }
+
+    public boolean isNotOperational() {
+        return notOperational;
+    }
+
+    public void setNotOperational(boolean notOperational) {
+        this.notOperational = notOperational;
+    }
+
+    public boolean isOperational() {
+        return operational;
+    }
+
+    public void setOperational(boolean operational) {
+        this.operational = operational;
+    }
+}
